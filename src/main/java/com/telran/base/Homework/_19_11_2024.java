33211d11de4f2d92d0ecc1d373a4824793c6e75a
KarpovYehor <karpovYehor@gmail.com>
2024-11-21 15:58:58 +0200

    Lesson1 + Homework

---
+++ src/main/java/com/telran/base/Homework/_19_11_2024.java
@@ -0,0 +1,11 @@
+package com.telran.base.Homework;
+
+public class _19_11_2024 {
+    public static void main(String[] args) {
+        System.out.println("Hello,");
+        System.out.println("my");
+        System.out.println("name");
+        System.out.println("is");
+        System.out.println("!");
+    }
+}
---
+++ src/main/java/com/telran/base/lesson1/Exemple.java
@@ -0,0 +1,11 @@
+package com.telran.base.lesson1;
+
+
+public class Exemple {
+
+    public static void main(String[] args) {
+        System.out.println("Привет Джава!!!");  // sout = System.out.println();
+
+
+    }
+}
